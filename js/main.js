f2e6a07f45a79483ff1731673a9ad178fb4b18f8
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-08-21 08:55:56 -0400

    Created final assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(event1) {
 
+    const button = document.querySelector('button')
+    button.addEventListener('click', function(event2) {
 
+        event2.target.classList.toggle('on')
+        event2.target.classList.toggle('off')
+
+        document.body.classList.toggle('dark')
+        document.body.classList.toggle('light')
+
+        const status = document.querySelector('h1.status')
+        status.textContent = document.body.classList.contains('light') ? "It's so bright in here!" : "Hey, who turned off the lights?"
+    })
+})
